f53fc54bef48779c371ced696535b9c671248097
Muhammad Zubair <mianmuhammadzubair94@gmail.com>
2026-03-16 11:04:49 +0500

    Pacakge setup

--- App.tsx
+++ App.tsx
@@ -17,7 +17,8 @@ function App() {
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar
+      barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <AppContent />
     </SafeAreaProvider>
   );
